ffcbe5fdc79ea6b1120d56a2ed995a9448c79f10
Amorim <g.amorim@campus.fct.unl.pt>
2024-03-26 23:43:08 +0000

    Up to F3

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/AuthToken.java
@@ -0,0 +1,26 @@
+package pt.unl.fct.di.apdc.firstwebapp.util;
+
+import java.util.UUID;
+
+public class AuthToken {
+	
+	public static final long EXPIRATION_TIME = 1000*60*60*2;
+	
+	public String username;
+	public String tokenID;
+	public long creationDate;
+	public long expirationDate;
+	
+	public AuthToken() {
+		
+	}
+	
+	public AuthToken(String username) {
+		this.username = username;
+		this.tokenID = UUID.randomUUID().toString();
+		this.creationDate = System.currentTimeMillis();
+		this.expirationDate = this.creationDate * AuthToken.EXPIRATION_TIME;
+	}
+	
+
+}
